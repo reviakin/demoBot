6612359b9c98b9a1358626041ef34a3b67b76a5e
reviakin <reviakindi@gmail.com>
2019-08-18 15:01:39 +0300

    add morgan

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+app.use(morgan())
 
 app.listen(3000, () => {
   console.log(`server start on 3000`)
