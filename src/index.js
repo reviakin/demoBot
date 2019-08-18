b04653f72d641afd2bf3a0ecab3e4a0314caaf95
reviakin <reviakindi@gmail.com>
2019-08-18 14:58:39 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 app.listen(3000, () => {
   console.log(`server start on 3000`)
