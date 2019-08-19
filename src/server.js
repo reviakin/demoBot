5d35a74feb2f8d84d60e3331577241184db7ffb7
reviakin <reviakindi@gmail.com>
2019-08-19 16:13:37 +0300

    174

--- src/server.js
+++ src/server.js
@@ -3,15 +3,15 @@ import { json, urlencoded } from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
 
-import config from './config'
 import { botStart } from './bot'
+import config from './config'
 
 const app = express()
 
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(cors())
-app.use(morgan())
+app.use(morgan('dev'))
 botStart()
 
 export const start = async () => {
