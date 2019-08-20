0aca3fda7e7ce93fe0360209edfc54d17aba4c3f
reviakin <reviakindi@gmail.com>
2019-08-20 12:05:18 +0300

    separated server & index files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,2 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import cors from 'cors'
-import morgan from 'morgan'
-
-import { botStart } from './bot'
-import config from './config'
-
-const app = express()
-
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(cors())
-app.use(morgan('dev'))
-
-botStart()
-
-try {
-  app.listen(config.port, () => {
-    console.log(`started in ${config.env} mode, on ${config.port}`)
-  })
-} catch (e) {
-  console.error(e)
-}
+import { start } from './server'
+start()
