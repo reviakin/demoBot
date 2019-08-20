0b2cb29ad6b48ebc0d6859e5841ded6d32c93aff
reviakin <reviakindi@gmail.com>
2019-08-20 10:08:42 +0300

    without async & try catch

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
 import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import cors from 'cors'
-import morgan from 'morgan'
+// import { json, urlencoded } from 'body-parser'
+// import cors from 'cors'
+// import morgan from 'morgan'
 
-import { botStart } from './bot'
+// import { botStart } from './bot'
 import config from './config'
 
 const app = express()
 
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(cors())
-app.use(morgan('dev'))
+// app.use(json())
+// app.use(urlencoded({ extended: true }))
+// app.use(cors())
+// app.use(morgan('dev'))
 
-botStart()
+// botStart()
 
-export const start = async () => {
-  try {
-    app.listen(config.port, () => {
-      console.log(`started in ${config.env} mode, on ${config.port}`)
-    })
-  } catch (e) {
-    console.error(e)
-  }
+export const start = () => {
+  // try {
+  app.listen(config.port, () => {
+    console.log(`started in ${config.env} mode, on ${config.port}`)
+  })
+  // } catch (e) {
+  // console.error(e)
+  // }
 }
