d4af6fe47c4665344bf0cf5445a0dfbeb492a768
reviakin <reviakindi@gmail.com>
2019-08-22 09:52:47 +0300

    add eng text

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -35,7 +35,7 @@ export const botStart = async () => {
       ) {
         bot.sendMessage(
           msg.chat.id,
-          `Уважаемый @${msg.from.username}, пожалуйста задайте вопрос, ответьте или напишите обращаясь к пользователю.`,
+          `Dear @${msg.from.username}, please ask a question, answer or write to the user`,
           { reply_to_message_id: msg.message_id }
         )
       }
